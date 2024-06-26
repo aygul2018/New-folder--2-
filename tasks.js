6d2cb34439da20f7cac6ec6b23112810dac863b1
aygul2018 <teacher.aygunm@gmail.com>
2024-06-26 19:05:35 +0400

    new

---
+++ tasks.js
@@ -0,0 +1,13 @@
+let age = prompt("how old are you?Your answers:17,16,15,18");
+ if(age =="18"){
+    console.log("allowed");
+ }
+ else if( age =="17" || age =="16" ||age =="15" ){
+    console.log("wasn t allowed");
+ }
+else{
+    console.log("this option doesn t exist");
+}
+
+
+
